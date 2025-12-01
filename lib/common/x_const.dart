942145af2d315b4f6ea073e4019917d003a12d31
ltarcher <59073821@qq.com>
2025-12-01 23:11:19 +0800

    Chore: 更新应用名称为"清岭AI

--- lib/common/x_const.dart
+++ lib/common/x_const.dart
@@ -1,13 +1,13 @@
 class XConst {
-  static final String name = "liu731/xiaozhi";
+  static final String name = "qingling_ai";
 
   static final double spacer = 12;
 
   static final double holdToTalkResponseAreaHeight = 100;
 
   static final String defaultOtaUrl =
-      'https://2662r3426b.vicp.fun/xiaozhi/ota/';
+      'https://qingling-ai.top/xiaozhi/ota/';
 
   static final String defaultWebsocketUrl =
-      'wss://2662r3426b.vicp.fun/xiaozhi/v1/';
+      'wss://qingling-ai.top/xiaozhi/v1/';
 }
