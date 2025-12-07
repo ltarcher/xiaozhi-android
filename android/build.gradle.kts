39f2911d1c04431b86552cec53dc5fce9565a092
ltarcher <59073821@qq.com>
2025-12-08 00:52:17 +0800

    Chore: 添加国内镜像源以加速依赖下载

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,5 +1,10 @@
 allprojects {
     repositories {
+        maven { url = uri("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/") }
+        maven { url = uri("https://mirrors.cloud.tencent.com/nexus/repository/gradle-plugins/") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/central") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
         google()
         mavenCentral()
     }
